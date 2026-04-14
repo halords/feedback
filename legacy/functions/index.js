215eb2cdfa0b9d9acb2285242575b1c0dee254b5
halords <haroldjamora08@gmail.com>
2026-04-14 15:22:48 +0800

    feat: initialize feedbackV2 application with new Next.js architecture and migrate legacy project assets

--- legacy/functions/index.js
+++
@@ -1,5 +0,0 @@
-global.Buffer = global.Buffer || require('buffer').Buffer;
-const functions = require('firebase-functions');
-const authApp = require('./auth/app');
-
-exports.api = functions.https.onRequest(authApp)
--- legacy/functions/test-express.js
+++
@@ -1,5 +0,0 @@
-process.env.FUNCTIONS_EMULATOR = 'true';
-const app = require('./auth/app');
-app.listen(9000, () => {
-    console.log('App successfully started on port 9000');
-});
